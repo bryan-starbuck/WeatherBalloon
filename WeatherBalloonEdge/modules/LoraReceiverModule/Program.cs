b800a467a4cdbe69c3a964766ca1908eb2a26a7c
unknown <bryan.starbuck@insight.com>
2018-10-09 21:34:09 -0700

    LoRa module work

--- WeatherBalloonEdge/modules/LoraReceiverModule/Program.cs
+++ WeatherBalloonEdge/modules/LoraReceiverModule/Program.cs
@@ -1,18 +1,27 @@
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Runtime.Loader;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+
+using RFMLib;
+    
 namespace LoraReceiverModule
 {
-    using System;
-    using System.IO;
-    using System.Runtime.InteropServices;
-    using System.Runtime.Loader;
-    using System.Security.Cryptography.X509Certificates;
-    using System.Text;
-    using System.Threading;
-    using System.Threading.Tasks;
-    using Microsoft.Azure.Devices.Client;
-
+    
     class Program
     {
-        static int counter;
+
+        private static ITransceiverSpiConnection _spiConnection;
+        private static RFM9XLoraTransceiver _rfm9XLoraTransceiver;
+
+        private static ModuleClient _ioTHubModuleClient;
+
+        private static System.Threading.Timer _heartbeatTimer;
 
         static void Main(string[] args)
         {
@@ -45,44 +54,123 @@ namespace LoraReceiverModule
             ITransportSettings[] settings = { amqpSetting };
 
             // Open a connection to the Edge runtime
-            ModuleClient ioTHubModuleClient = await ModuleClient.CreateFromEnvironmentAsync(settings);
-            await ioTHubModuleClient.OpenAsync();
+            _ioTHubModuleClient = await ModuleClient.CreateFromEnvironmentAsync(settings);
+            await _ioTHubModuleClient.OpenAsync();
             Console.WriteLine("IoT Hub module client initialized.");
 
             // Register callback to be called when a message is received by the module
-            await ioTHubModuleClient.SetInputMessageHandlerAsync("input1", PipeMessage, ioTHubModuleClient);
+            await _ioTHubModuleClient.SetInputMessageHandlerAsync("transmitterInput", TransmitIoTMessageWitLora, _ioTHubModuleClient);
+
+            InitializeReceiver();
+
+            Console.WriteLine("Starting Heartbeat timer");
+            _heartbeatTimer = new Timer(new TimerCallback(HeartbeatCallback), null, new TimeSpan(0, 0, 0), new TimeSpan(0,1,0));
         }
 
-        /// <summary>
-        /// This method is called whenever the module is sent a message from the EdgeHub. 
-        /// It just pipe the messages without any change.
-        /// It prints all the incoming messages.
-        /// </summary>
-        static async Task<MessageResponse> PipeMessage(Message message, object userContext)
+        public static void HeartbeatCallback(object state)
         {
-            int counterValue = Interlocked.Increment(ref counter);
+            byte[] messageBytes = Encoding.UTF8.GetBytes("Lora Heartbeat message: " + DateTime.UtcNow.ToString());
 
+            Console.WriteLine("Sending Lora Heartbeat");
+            Task.Run( () => TransmitLoraMessage(messageBytes) );
+        }
+
+        static async Task<MessageResponse> TransmitIoTMessageWitLora(Message message, object userContext)
+        {
             var moduleClient = userContext as ModuleClient;
             if (moduleClient == null)
             {
-                throw new InvalidOperationException("UserContext doesn't contain " + "expected values");
+                throw new InvalidOperationException("UserContext doesn't contain expected values");
             }
 
             byte[] messageBytes = message.GetBytes();
             string messageString = Encoding.UTF8.GetString(messageBytes);
-            Console.WriteLine($"Received message: {counterValue}, Body: [{messageString}]");
+            Console.WriteLine($"Received message: Body: [{messageString}]");
 
-            if (!string.IsNullOrEmpty(messageString))
-            {
-                var pipeMessage = new Message(messageBytes);
-                foreach (var prop in message.Properties)
-                {
-                    pipeMessage.Properties.Add(prop.Key, prop.Value);
-                }
-                await moduleClient.SendEventAsync("output1", pipeMessage);
-                Console.WriteLine("Received message sent");
-            }
+            await TransmitLoraMessage(messageBytes);
+
             return MessageResponse.Completed;
+        }
+
+        private static void InitializeReceiver()
+        {
+            try 
+            {
+                TrancieverConnectionFactory trancieverConnectionFactory = new TrancieverConnectionFactory();
+
+                _spiConnection = trancieverConnectionFactory.CreateForDragino();
+                _rfm9XLoraTransceiver = new RFM9XLoraTransceiver(_spiConnection);
+
+                _rfm9XLoraTransceiver.Initialize();
+
+                Task.Run(() => ReceiveLoraMessages());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to initialize Lora Receiver: " + ex.Message);
+            }
+        }
+
+        private static void ReceiveLoraMessages()
+        {
+            while (true)
+            {
+                Task<RawData> receiveTask = _rfm9XLoraTransceiver.Recieve();
+
+                receiveTask.Wait();
+
+                if (receiveTask.Result != null)
+                {
+                    Console.WriteLine("Received Lora Message: " + Encoding.ASCII.GetString(receiveTask.Result.Buffer));
+
+                    Task.Run(() => SendIoTMessage(receiveTask.Result.Buffer));
+                }
+            }
+        }
+
+        private static async Task<bool> TransmitLoraMessage(Byte [] messageBytes)
+        {
+            try 
+            {
+
+                var success = await _rfm9XLoraTransceiver.Transmit(messageBytes);
+                
+                if (!success)
+                {
+                    Console.WriteLine("Transmit Failed");
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to Transmit Lora Message: " + ex.Message);
+                return false;
+            }
+
+            Console.WriteLine("Transmit Success");
+            return true;
+        }
+
+        private static async Task<bool> SendIoTMessage(Byte[] messageBytes)
+        {
+            try 
+            {
+                if (_ioTHubModuleClient != null)
+                {
+                    var iotMessage = new Message(messageBytes);
+
+                    await _ioTHubModuleClient.SendEventAsync("receiverOutput", iotMessage);
+
+                    Console.WriteLine("IoT message sent");
+                }
+            }
+            catch ( Exception ex )
+            {
+                Console.WriteLine("Failed to send iot message: "+ ex.Message);
+                return false;
+            }
+
+            return true;
         }
     }
 }
---
+++ WeatherBalloonEdge/utility/HopeRF.NET/RaspberrySharp.Core/Raspberry.IO.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Raspberry.IO.Core
+{
+    public class Class1
+    {
+    }
+}
